8130dfb7e0d62b1f674850da3c3c5c98e4df5518
Yetunde Sola-Adebayo <ysolaadebayo@gmail.com>
2018-08-07 17:25:08 -0500

    continue design of profile page

--- resources/scripts/profile.php
+++ resources/scripts/profile.php
@@ -14,39 +14,85 @@
 </head>
 
 <body>
-  
+
+<nav class="navbar fixed-top navbar-expand-lg navbar-light">
+
+<div id="logolink">
+    <a href="index.html">
+        <img id="logo" src="/resources/img/shootlogo.png">
+    </a>
+</div>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+    aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<!--button for mobile view-->
+
+<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+        <!--list for navbar-->
+
+        <li class="nav-item">
+            <a class="nav-link nav-a" href="index.html">Welcome
+                <span class="sr-only">(current)</span>
+            </a>
+        </li>
+
+
+        <li class="nav-item">
+            <a class="nav-link nav-a" href="about.html">About Us</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="signup.html">
+                <button class="btn">Sign Up</button>
+            </a>
+        </li>
+    </ul>
+</div>
+
+</nav>
+
+
+<!--Photographer profile-->
     <div class="box">
         <div id="color_top"></div>
         
         <div id="box_container">
             <div id="info">
-                <div id="profile_picture">
-                    <img src="/resources/img/pic.jpg">
+                <div id="profile_picture">   <!--photographer image-->
+                    <img class="border_img" src="/resources/img/pic.jpg">
                 </div>
 
-                <div id="profile_info">
+                <div id="profile_info"> <!--photographer info-->
                     <h1>Lisa Martinez</h1>
-                    <h5>Price Range: $100 - $500</h5>
-                    <h5>Location: Houston</h5>
-                    <button id="book">Book this photographer</button>
+                    <h5 class="info_text">Price Range: $100 - $500</h5>
+                    <h5 class="info_text">Location: Houston</h5>
+                    <button id="book"><a class="button_hover" href="/index.php">Book this photographer</a></button>
                 </div>
             </div>
 
-            <div id="portfolio_container">
+            <div id="portfolio_container"> <!--porfolio-->
                 <h2>Portfolio</h2>
 
-                <div class="portfolio"> 
-                    <div><img src="/resources/img/pic.jpg"></div>
-                    <div><img src="/resources/img/pic.jpg"></div>
-                    <div><img src="/resources/img/pic.jpg"></div>
-                    <div><img src="/resources/img/pic.jpg"></div>
-                    <div><img src="/resources/img/pic.jpg"></div>
+                <div id="portfolio">   <!--portfolio images-->
+                    <div><img class="border_img" src="/resources/img/pic.jpg"></div>
+                    <div><img class="border_img" src="/resources/img/pic.jpg"></div>
+                    <div><img class="border_img" src="/resources/img/pic.jpg"></div>
+                    <div><img class="border_img" src="/resources/img/pic.jpg"></div>
+                    <div><img class="border_img" src="/resources/img/pic.jpg"></div>
+                    <div><img class="border_img" src="/resources/img/pic.jpg"></div>
+                    <div><img class="border_img" src="/resources/img/pic.jpg"></div>
+                    <div><img class="border_img" src="/resources/img/pic.jpg"></div>
+                    <div><img class="border_img" src="/resources/img/pic.jpg"></div>
+                 
                 </div> 
             </div>
         </div>
 
-        <div id="back_container">
-            <button id="back">Back</button>
+        <div id="back_container">  <!--back buttton-->
+            <button id="back"><a class="button_hover" href="#">Back</a></button>
         </div>
         
     </div>
